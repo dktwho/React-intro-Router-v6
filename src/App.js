7cffb47bb6b402d914564b27dd027eecb73f581c
Alexander Korobov <dktwho@mail.ru>
2023-03-17 12:38:47 +0300

    add components, and example router v5 for refactoring to v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, NavLink, Link} from 'react-router-dom'
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contacts from './components/Contacts/Contacts';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <NavLink exact to="/" activeStyle={{fontWeight: "bold",    color: "red"  }} >Home</NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/about" activeStyle={{fontWeight: "bold",    color: "red"  }} >About</NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/dashboard" activeStyle={{fontWeight: "bold",    color: "red"  }} >Dashboard</NavLink>
+          </li>
+        </ul>
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Contacts />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
