19ccef5b69800f691a7ba7361fac9cb6cbd95502
Alexander Korobov <dktwho@mail.ru>
2023-03-17 13:10:54 +0300

    added style isActive for NavLink

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch, NavLink, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contacts from './components/Contacts/Contacts';
 
+
 function App() {
+
+  const isActiveStyle = ({isActive}) => ({
+    color : isActive ? 'green': 'blue'
+  })
+
   return (
     <Router>
       <div>
         <ul>
           <li>
-            <NavLink exact to="/" activeStyle={{fontWeight: "bold",    color: "red"  }} >Home</NavLink>
+            <NavLink end  to="/" style={isActiveStyle} >Home</NavLink>
           </li>
           <li>
-            <NavLink exact to="/about" activeStyle={{fontWeight: "bold",    color: "red"  }} >About</NavLink>
+            <NavLink end  to="/about" style={isActiveStyle} >About</NavLink>
           </li>
           <li>
-            <NavLink exact to="/dashboard" activeStyle={{fontWeight: "bold",    color: "red"  }} >Dashboard</NavLink>
+            <NavLink end  to="/contacts" style={isActiveStyle}>Contacts</NavLink>
           </li>
         </ul>
         <hr />
 
-        <Switch>
-          <Route exact path="/">
-            <Home />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={     <About />}/>
+          <Route path="/contacts" element={ <Contacts />}>
           </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Contacts />
-          </Route>
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
